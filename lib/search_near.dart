fcd753959212982a54ac8877e799d0ae0ba3a050
shroida <shroida012@gmail.com>
2025-12-27 14:52:56 +0200

    get the app ready

---
+++ lib/search_near.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class SearchNear extends StatelessWidget {
+const SearchNear({ super.key });
+
+  @override
+  Widget build(BuildContext context){
+    return Container();
+  }
+}
