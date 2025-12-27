de4b19f71ea21f6517acadc7371cfbc34f643a5e
shroida <shroida012@gmail.com>
2025-12-27 15:05:21 +0200

    starting with creating first screen

--- lib/search_near.dart
+++ lib/search_near.dart
@@ -1,10 +1,19 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class SearchNear extends StatelessWidget {
-const SearchNear({ super.key });
+  const SearchNear({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return Container();
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        body: Center(child: Text('Shroida')),
+      ),
+    );
   }
 }
