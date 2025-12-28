4ccd8d6be3bb82740d25354e71bd70396853b731
shroida <shroida012@gmail.com>
2025-12-28 23:06:38 +0200

    create pages

--- lib/presentation/common/core/app_router.dart
+++ lib/presentation/common/core/app_router.dart
@@ -1,17 +1,13 @@
 import 'package:go_router/go_router.dart';
 import 'package:servicenear/presentation/common/core/routes_path.dart';
 
-final GoRouter AppRouter = GoRouter(
-  initialLocation: '/',
-  routes:[
-     GoRoute(
-      path: RoutePath.login,
-      builder: (context, state) => const LoginScreen(),
-    ),
-    //  GoRoute(
-    //   path: '/',
-    //   builder: (context, state) => const HomePage(),
-    // ),
-  ]
-
-);
+final GoRouter appRouter = GoRouter(initialLocation: '/', routes: [
+  GoRoute(
+    path: RoutePath.login,
+    builder: (context, state) => const LoginScreen(),
+  ),
+  //  GoRoute(
+  //   path: '/',
+  //   builder: (context, state) => const HomePage(),
+  // ),
+]);
