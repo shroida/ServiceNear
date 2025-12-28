587803be50c483dcc25a286556d0b6279e1d54c1
shroida <shroida012@gmail.com>
2025-12-28 23:04:11 +0200

    route path

--- lib/presentation/common/core/app_router.dart
+++ lib/presentation/common/core/app_router.dart
@@ -1,10 +1,11 @@
 import 'package:go_router/go_router.dart';
+import 'package:servicenear/presentation/common/core/routes_path.dart';
 
 final GoRouter AppRouter = GoRouter(
   initialLocation: '/',
   routes:[
      GoRoute(
-      path: '/',
+      path: RoutePath.login,
       builder: (context, state) => const LoginScreen(),
     ),
     //  GoRoute(
---
+++ lib/presentation/common/core/routes_path.dart
@@ -0,0 +1,6 @@
+class RoutePath {
+  static const String home = '/home';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String profile = '/profile';
+}
