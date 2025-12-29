80e4facadedd055d136514a1dab24e7cb060f392
shroida <shroida012@gmail.com>
2025-12-29 09:32:36 +0200

    call it in test folder

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -6,14 +6,14 @@
 // tree, read text, and verify that the values of widget properties are correct.
 
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:servicenear/search_near.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const SearchNear());
+    await tester.pumpWidget(const ScreenUtilInit());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
