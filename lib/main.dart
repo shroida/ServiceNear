731052139ef174d164cc21284bfb0506d9597473
shroida <shroida012@gmail.com>
2025-12-29 09:31:29 +0200

    make it faster cleaner

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:servicenear/presentation/common/core/app_router.dart';
 import 'package:servicenear/search_near.dart';
 
 void main() {
   final appRouter = AppRouter();
 
-  runApp(SearchNear(appRouter: appRouter.router));
+  runApp(
+    ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, __) => SearchNear(
+        appRouter: appRouter.router,
+      ),
+    ),
+  );
 }
--- lib/search_near.dart
+++ lib/search_near.dart
@@ -1,31 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:servicenear/presentation/common/core/app_colors.dart';
 
 class SearchNear extends StatelessWidget {
-  const SearchNear({super.key, required this.appRouter});
+  const SearchNear({
+    super.key,
+    required this.appRouter,
+  });
+
   final GoRouter appRouter;
+
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: const Size(375, 812),
-      minTextAdapt: true,
-      splitScreenMode: true,
-      builder: (context, child) {
-        return MaterialApp.router(
-          routerConfig: appRouter,
-          title: 'Service Near',
-          theme: ThemeData(
-            primaryColor: AppColors.primary,
-            scaffoldBackgroundColor: Colors.white,
-          ),
-          routerDelegate: appRouter.routerDelegate,
-          routeInformationParser: appRouter.routeInformationParser,
-          routeInformationProvider: appRouter.routeInformationProvider,
-          debugShowCheckedModeBanner: false,
-        );
-      },
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      title: 'Service Near',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: AppColors.primary,
+        scaffoldBackgroundColor: Colors.white,
+        useMaterial3: true,
+      ),
     );
   }
 }
