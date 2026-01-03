429e8496526a68839159db655a5f8d4c982b4960
shroida <shroida012@gmail.com>
2026-01-03 21:19:16 +0200

    AuthRemoteDataSource

---
+++ lib/features/auth/data/datasources/auth_remote_datasource.dart
@@ -0,0 +1,16 @@
+abstract class AuthRemoteDataSource {
+  Future<void> signIn({
+    required String email,
+    required String password,
+  });
+
+  Future<void> signUp({
+    required String email,
+    required String password,
+    required String fullName,
+  });
+
+  Future<void> signOut();
+
+  Future<bool> isLoggedIn();
+}
