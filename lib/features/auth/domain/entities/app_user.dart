501f6058e3705ca1e156949816f4868410330ab3
shroida <shroida012@gmail.com>
2026-01-03 10:04:47 +0200

    make app user abstract to be depended on

--- lib/features/auth/domain/entities/app_user.dart
+++ lib/features/auth/domain/entities/app_user.dart
@@ -1,7 +1,7 @@
 import 'package:servicenear/features/auth/data/models/user_location.dart';
-import 'package:servicenear/features/auth/domain/entities/user_type.dart';
+import 'user_type.dart';
 
-class AppUser {
+abstract class AppUser {
   final String id;
   final String firstName;
   final String lastName;
@@ -19,28 +19,4 @@ class AppUser {
     required this.location,
     required this.createdAt,
   });
-
-  factory AppUser.fromJson(Map<String, dynamic> json) {
-    return AppUser(
-      id: json['id'],
-      firstName: json['first_name'],
-      lastName: json['last_name'],
-      email: json['email'],
-      userType: json['user_type'] == 'worker' ? UserType.worker : UserType.customer,
-      location: UserLocation.fromJson(json),
-      createdAt: DateTime.parse(json['created_at']),
-    );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'id': id,
-      'first_name': firstName,
-      'last_name': lastName,
-      'email': email,
-      'user_type': userType.name,
-      ...location.toJson(), // 🔥 merge location fields
-      'created_at': createdAt.toIso8601String(),
-    };
-  }
 }
