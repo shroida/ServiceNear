95c3c6824c2bc38f2e8eb0c9c3dc2159319401ba
Shroida Official <shroida012@gmail.com>
2026-01-30 23:40:16 +0200

    AppUserHome

---
+++ lib/features/home/domain/entities/app_user_home.dart
@@ -0,0 +1,13 @@
+import 'package:servicenear/common/entities/app_user.dart';
+
+class AppUserHome extends AppUser {
+  AppUserHome({
+    required super.id,
+    required super.firstName,
+    required super.lastName,
+    required super.email,
+    required super.userType,
+    required super.location,
+    required super.createdAt,
+  });
+}
