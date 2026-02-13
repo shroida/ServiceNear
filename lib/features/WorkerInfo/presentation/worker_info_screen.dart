2a0fc2baf44fb429bdae3e3b0941d4a7b2224d52
Shroida Official <shroida012@gmail.com>
2026-02-13 21:04:39 +0200

    add WorkerInfo

--- lib/features/WorkerInfo/presentation/worker_info_screen.dart
+++ lib/features/WorkerInfo/presentation/worker_info_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:servicenear/features/WorkerInfo/presentation/widgets/about_widget.dart';
 import 'package:servicenear/features/WorkerInfo/presentation/widgets/info_card.dart';
+import 'package:servicenear/features/WorkerInfo/presentation/widgets/rating_section.dart';
 import 'package:servicenear/features/WorkerInfo/presentation/widgets/worker_info_header.dart';
 import 'package:servicenear/common/entities/app_user.dart';
 import 'package:servicenear/common/core/app_colors.dart';
@@ -30,7 +31,8 @@ class WorkerInfoScreen extends StatelessWidget {
 
           SizedBox(height: 5.h),
           InfoCard(user: user, specialty: specialty),
-          SizedBox(height: 20.h),
+          SizedBox(height: 5.h),
+          RatingSection(rating: 5.5, reviewsCount: 15),
         ],
       ),
     );
