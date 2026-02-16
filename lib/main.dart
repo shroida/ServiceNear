75d5b54b3e78fc8fa8be8b4f4cc04d79d88a190f
Shroida Official <shroida012@gmail.com>
2026-02-16 09:04:39 +0200

    add DI

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:servicenear/common/core/app_router.dart';
+import 'package:servicenear/common/core/di/injection.dart';
 import 'package:servicenear/common/core/supabase_client.dart';
 import 'package:servicenear/search_near.dart';
 
@@ -9,7 +10,7 @@ void main() async {
 
   await SupabaseManager.init();
   final appRouter = AppRouter();
-
+  await init();
   runApp(
     ScreenUtilInit(
       designSize: const Size(375, 812),
