560ddafb770684f8154629b26499719b10326707
Shroida Official <shroida012@gmail.com>
2026-02-20 15:52:00 +0200

    getMessages

--- lib/features/chat/domain/repositories/chat_repositories.dart
+++ lib/features/chat/domain/repositories/chat_repositories.dart
@@ -2,4 +2,5 @@ import 'package:servicenear/features/chat/domain/entites/message_entity.dart';
 
 abstract class ChatRepository {
   Future<void> sendMessage(MessageEntity message);
+  Future<List<MessageEntity>> getMessages(String senderId);
 }
