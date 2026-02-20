af69ce6f39a9e688550086a760b70d9a8332cc0d
Shroida Official <shroida012@gmail.com>
2026-02-20 04:42:56 +0200

    ChatRepositoryImpl

---
+++ lib/features/chat/data/repositories/chat_repostories_impl.dart
@@ -0,0 +1,11 @@
+import 'package:servicenear/features/chat/domain/entites/message_entity.dart';
+import 'package:servicenear/features/chat/domain/repositories/chat_repositories.dart';
+
+class ChatRepositoryImpl implements ChatRepository {
+  @override
+  Future<void> sendMessage(MessageEntity message) {
+
+  }
+  @override
+
+}
