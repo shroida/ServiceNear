326fe2adb7e6ee132abb7ec68240480673e6f315
Shroida Official <shroida012@gmail.com>
2026-02-20 05:12:56 +0200

    ChatState

---
+++ lib/features/chat/presentation/cubit/chat_state.dart
@@ -0,0 +1,12 @@
+abstract class ChatState {}
+
+class ChatInitial extends ChatState {}
+
+class ChatLoading extends ChatState {}
+
+class ChatSuccess extends ChatState {}
+
+class ChatError extends ChatState {
+  final String message;
+  ChatError(this.message);
+}
