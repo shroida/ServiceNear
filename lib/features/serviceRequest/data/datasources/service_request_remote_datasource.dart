761a24348a9a7ed6af7d67527623cec79501a9e7
Shroida Official <shroida012@gmail.com>
2026-03-06 23:53:30 +0200

    create impl datasource

--- lib/features/serviceRequest/data/datasources/service_request_remote_datasource.dart
+++ lib/features/serviceRequest/data/datasources/service_request_remote_datasource.dart
@@ -1,43 +1,8 @@
-import 'package:servicenear/features/serviceRequest/data/models/service_request_model.dart';
 import 'package:servicenear/features/serviceRequest/domain/entities/service_request.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
-class ServiceRequestRemoteDataSource {
-  final SupabaseClient client;
-
-  ServiceRequestRemoteDataSource(this.client);
-
-  Future<void> createRequest(Map<String, dynamic> data) async {
-    await client.from('service_requests').insert(data);
-  }
-
-  Future<List<ServiceRequest>> getWorkerRequests(String workerId) async {
-    final res = await client
-        .from('service_requests')
-        .select()
-        .eq('worker_id', workerId);
-
-    return res.map<ServiceRequest>((e) {
-      return ServiceRequestModel.fromMap(e);
-    }).toList();
-  }
-
-  Future<List<ServiceRequest>> getCustomerRequests(String customerId) async {
-    final res = await client
-        .from('service_requests')
-        .select('*')
-        .eq('customer_id', customerId)
-        .order('created_at', ascending: false);
-
-    return res.map<ServiceRequest>((e) {
-      return ServiceRequestModel.fromMap(e);
-    }).toList();
-  }
-
-  Future<void> updateRequestStatus(String requestId, String status) async {
-    await client
-        .from('service_requests')
-        .update({'status': status})
-        .eq('id', requestId);
-  }
+abstract class ServiceRequestRemoteDataSource {
+  Future<List<ServiceRequest>> getWorkerRequests(String workerId);
+  Future<List<ServiceRequest>> getCustomerRequests(String customerId);
+  Future<void> createRequest(Map<String, dynamic> data);
+  Future<void> updateRequestStatus(String requestId, String status);
 }
---
+++ lib/features/serviceRequest/data/datasources/service_request_remote_datasource_impl.dart
@@ -0,0 +1,49 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+import '../../domain/entities/service_request.dart';
+import '../models/service_request_model.dart';
+import 'service_request_remote_datasource.dart';
+
+class ServiceRequestRemoteDataSourceImpl
+    implements ServiceRequestRemoteDataSource {
+  final SupabaseClient client;
+
+  ServiceRequestRemoteDataSourceImpl(this.client);
+
+  @override
+  Future<void> createRequest(Map<String, dynamic> data) async {
+    await client.from('service_requests').insert(data);
+  }
+
+  @override
+  Future<List<ServiceRequest>> getWorkerRequests(String workerId) async {
+    final res = await client
+        .from('service_requests')
+        .select()
+        .eq('worker_id', workerId);
+
+    return res
+        .map<ServiceRequest>((e) => ServiceRequestModel.fromMap(e))
+        .toList();
+  }
+
+  @override
+  Future<List<ServiceRequest>> getCustomerRequests(String customerId) async {
+    final res = await client
+        .from('service_requests')
+        .select()
+        .eq('customer_id', customerId)
+        .order('created_at', ascending: false);
+
+    return res
+        .map<ServiceRequest>((e) => ServiceRequestModel.fromMap(e))
+        .toList();
+  }
+
+  @override
+  Future<void> updateRequestStatus(String requestId, String status) async {
+    await client
+        .from('service_requests')
+        .update({'status': status})
+        .eq('id', requestId);
+  }
+}
