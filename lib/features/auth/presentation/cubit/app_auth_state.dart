9f799a6c14d8e03f9b07ab4cb00a7a50d8fe852a
Shroida Official <shroida012@gmail.com>
2026-03-08 06:45:59 +0200

    refactor for app router to not be depended on repo

--- lib/features/auth/presentation/cubit/app_auth_state.dart
+++ lib/features/auth/presentation/cubit/app_auth_state.dart
@@ -1,8 +1,18 @@
 import 'package:servicenear/common/entities/app_user.dart';
 
-abstract class AppAuthState {}
-
 class AuthInitial extends AppAuthState {}
+
+abstract class AppAuthState {
+  String get currentUserId => '';
+}
+
+class AuthAuthenticated extends AppAuthState {
+  final AppUser user;
+  AuthAuthenticated(this.user);
+
+  @override
+  String get currentUserId => user.id;
+}
 
 class AuthLoading extends AppAuthState {}
 
