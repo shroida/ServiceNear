5892e251275060e07f977359abce291bbb195191
Shroida Official <shroida012@gmail.com>
2026-03-14 11:39:17 +0200

    refactor supabase

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:servicenear/common/core/app_router.dart';
 import 'package:servicenear/common/core/di/injection.dart';
 import 'package:servicenear/common/core/supabase_client.dart';
 import 'package:servicenear/features/auth/presentation/cubit/auth_cubit.dart';
+import 'package:servicenear/search_near.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,13 +17,12 @@ void main() async {
 
   runApp(
     BlocProvider(
-      create: (_) => sl<AuthCubit>(), // Provide AuthCubit here
+      create: (_) => sl<AuthCubit>(),
       child: ScreenUtilInit(
         designSize: const Size(375, 812),
         minTextAdapt: true,
         splitScreenMode: true,
-        builder: (context, child) =>
-            MaterialApp.router(routerConfig: appRouter.router),
+        builder: (_, _) => SearchNear(appRouter: appRouter.router),
       ),
     ),
   );
