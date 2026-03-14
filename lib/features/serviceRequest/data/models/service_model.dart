a514dab96dd2b26e23556b379f88c136ca73781c
Shroida Official <shroida012@gmail.com>
2026-03-14 13:06:24 +0200

    create ServiceModel

---
+++ lib/features/serviceRequest/data/models/service_model.dart
@@ -0,0 +1,31 @@
+import '../../domain/entities/service.dart';
+
+class ServiceModel extends Service {
+  ServiceModel({
+    super.id,
+    required super.title,
+    required super.description,
+    required super.price,
+    required super.specialty,
+  });
+
+  factory ServiceModel.fromMap(Map<String, dynamic> map) {
+    return ServiceModel(
+      id: map['id'],
+      title: map['title'],
+      description: map['description'] ?? '',
+      price: map['price']?.toDouble() ?? 0.0,
+      specialty: map['specialty'] ?? '',
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      if (id != null) 'id': id,
+      'title': title,
+      'description': description,
+      'price': price,
+      'specialty': specialty,
+    };
+  }
+}
